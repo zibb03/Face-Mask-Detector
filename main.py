c3513f3a1e99a043435af9ade9f8e55717c54d06
zibb03 <64014435+zibb03@users.noreply.github.com>
2021-04-12 20:56:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,23 @@ label = QLabel()
 label.show()
 
 app.exec_()
+
+'''
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('My First Application')
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   sys.exit(app.exec_())
+'''
